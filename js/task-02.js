f95df158cf872d17391a5ff7fb5af6a410fda0a0
Artem Sydorov <sydorovay@gmail.com>
2023-03-07 01:23:35 +0100

    finished task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const completeTheList = ingredients.map((ingredient) => {
+  const items = document.createElement("li");
+  items.textContent = ingredient;
+  items.classList.add("item");
+  list.append(items);
+});
